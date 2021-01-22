4c2f92fa20e905c0ad37c20b77d389c93f078b6d
Chris Laffra <laffra@gmail.com>
2021-01-23 00:09:04 +0100

    Only show progressbar when time is set

--- hurry/contentscript.js
+++ hurry/contentscript.js
@@ -1,6 +1,6 @@
 (function Hurry() {
     const settings = {
-        time: 45,
+        time: 0,
         normal: "green",
         warning: "orange",
         hurry: "red",
@@ -10,7 +10,7 @@
         width: 200,
         height: 5,
         border: "1px solid grey",
-        start: Date.now(),
+        start: Date.now()
     };
     const urlParts = document.location.pathname.split("/");
     const docId = urlParts[urlParts.length - 2];
@@ -23,15 +23,33 @@
         }
     }
 
+    function syncSettings() {
+        for (const key in settings) {
+            const value = localStorage.getItem("hurry-" + docId + "-" + key);
+            settings[key] = (value === null) ? settings[key] : value;
+        }
+    }
+
+    function clearSettings() {
+        for (const key in settings) {
+            localStorage.removeItem("hurry-" + docId + "-" + key);
+        }
+    }
+
+    syncSettings();
+
     function update() {
-        const startMillis = settings["start"];
+        if (settings.time < 1) return;
+        $("#hurry-progressbar")
+            .css("visibility", "visible");
+        const startMillis = settings.start;
         const nowMillis = Date.now();
         const elapsedSeconds = (nowMillis - startMillis) / 1000;
-        const totalSeconds = (settings["time"] * 60);
+        const totalSeconds = (settings.time * 60);
         const ratio = elapsedSeconds / totalSeconds;
-        const width = Math.min(settings["width"], settings["width"] * ratio);
-        const color = ratio > 0.5 ? ratio > 0.75 ? settings["hurry"] : settings["warning"] : settings["normal"];
-        $("#hurry-bar")
+        const width = Math.min(settings.width, settings.width * ratio);
+        const color = ratio > 0.5 ? ratio > 0.75 ? settings.hurry : settings.warning : settings.normal;
+        $("#hurry-innerbar")
             .css("background", color)
             .css("width", width + "px");
         const secondsLeft = Math.round(Math.max(0, totalSeconds - elapsedSeconds));
@@ -41,54 +59,50 @@
     }
 
     function showTimer() {
-        if (!$("#hurry-timer").length) {
-            for (const key in settings) {
-                settings[key] = localStorage.getItem("hurry-" + docId + "-" + key) || settings[key];
-            }
+        if (!$("#hurry-progressbar").length) {
+            syncSettings();
             $("<div>")
-                .attr("id", "hurry-timer")
+                .attr("id", "hurry-progressbar")
+                .css("visibility", "hidden")
                 .css("position", "absolute")
-                .css("top", settings["top"] + "px")
-                .css("right", settings["right"] + "px")
-                .css("width", settings["width"] + "px")
-                .css("height", settings["height"] + "px")
-                .css("border", settings["border"])
+                .css("top", settings.top + "px")
+                .css("right", settings.right + "px")
+                .css("width", settings.width + "px")
+                .css("height", settings.height + "px")
+                .css("border", settings.border)
                 .appendTo($("body"))
                 .append($("<div>")
                     .attr("id", "hurry-label")
                     .css("font-family", "Arial")
-                    .css("font-size", (settings["height"] * 2) + "px")
+                    .css("font-size", (settings.height * 2) + "px")
                     .css("position", "absolute")
                     .css("top", "-14px")
                     .css("right", "0px")
-                    .css("color", settings["label"]))
+                    .css("color", settings.label))
                 .append($("<div>")
-                    .attr("id", "hurry-bar")
-                    .css("background", settings["normal"])
+                    .attr("id", "hurry-innerbar")
+                    .css("background", settings.normal)
                     .css("width", "0px")
-                    .css("height", settings["height"] + "px"));
-            settings["start"] = Date.now();
+                    .css("height", settings.height + "px"));
+            settings.start = Date.now();
             setInterval(update, 1000);
         }
     }
 
     function loadNotes() {
+        clearSettings();
         var notes = $("#speakernotes").text();
         if (!notes) return;
         const pageNumber = parseInt($(".punch-filmstrip-selected-thumbnail-pagenumber").text());
         if (pageNumber != 1) return;
         const assignments = notes.split("#");
-        for (const key in settings) {
-            localStorage.removeItem("hurry-" + docId + "-" + key);
-        }
+        syncSettings();
         for (const assignment of assignments) {
             const parts = assignment.split("=");
             const name = parts[0];
             if (!name) continue;
             const value = isNaN(parseInt(parts[1])) ? parts[1] : parseInt(parts[1]);
-            if (settings[name]) {
-                localStorage.setItem("hurry-" + docId + "-" + name, value);
-            }
+            localStorage.setItem("hurry-" + docId + "-" + name, value);
         }
     }
 
